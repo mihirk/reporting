df2137ca5c02ad971e6a9d086ba1891f70ff5a8c
mihirk <mihir.khatwani@gmail.com>
2014-05-18 06:15:17 +0530

    Mihir | Tests passing, can remove debugs now

--- run_tests.sh
+++ run_tests.sh
@@ -3,10 +3,9 @@ set -e
 curl -L https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.1.1.tar.gz > elasticsearch.tar.gz
 tar -xvf elasticsearch.tar.gz
 bash elasticsearch-1.1.1/bin/elasticsearch -d
-echo "Sleeping for 20 seconds"
-sleep 20
+echo "Sleeping for 5 seconds"
+sleep 5
 echo "Back up again :)"
-ps -eaf | grep elasticsearch
 python -m unittest discover
 status=$?
 if [ $status -ne 0 ]; then
